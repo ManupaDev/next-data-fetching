13fc1f3caa0fddd0170b87e452760f2456b06cf4
ManupaDev <manupadevsuite@gmail.com>
2022-12-03 02:34:32 +0530

    data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
+export default async function Home() {
 
-export default function Home() {
+  interface Post {
+    userId:number;
+    id:number;
+    title:string;
+    body:string;
+  }
+
+  const posts:Post[] = await (await fetch('https://jsonplaceholder.typicode.com/posts')).json();
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="p-4 grid grid-cols-3 gap-4">
+      {
+        posts.map((post,i)=>{
+          return(
+            <div key={i} className="border border-black">
+              <h1>UserId:{post.userId}</h1>
+              <h1>PostId:{post.id}</h1>
+              <h1>{post.title}</h1>
+              <h1>{post.body}</h1>
+            </div>
+          );
+        })
+      }
     </div>
   )
 }
+
