613a3f086edc36d6ae1dc88e84685ba711e6847c
ManupaDev <manupadevsuite@gmail.com>
2022-12-03 18:49:33 +0530

    test staic generation

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import UserBar from './user-bar';
+import UserBar from "./user-bar";
 
 export default async function Home() {
   type Post = {
@@ -6,23 +6,28 @@ export default async function Home() {
     id: number;
     title: string;
     body: string;
-  }
+  };
+
+  const num = Math.random() * 100;
 
   const posts: Post[] = await (
     await fetch("https://jsonplaceholder.typicode.com/posts")
   ).json();
 
   return (
-    <div className="p-4 grid grid-cols-3 gap-4">
-      {posts.map((post, i) => {
-        return (
-          <div key={i} className="border border-black">
-            <UserBar userId={post.userId}/>
-            <h1 className='text-2xl font-semibold'>{post.title}</h1>
-            <h1>{post.body}</h1>
-          </div>
-        );
-      })}
+    <div className="p-4 ">
+      <h1>{num}</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {posts.map((post, i) => {
+          return (
+            <div key={i} className="border border-black">
+              <UserBar userId={post.userId} />
+              <h1 className="text-2xl font-semibold">{post.title}</h1>
+              <h1>{post.body}</h1>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
